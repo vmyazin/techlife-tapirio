e808f08e76963e13c7992569888d1b5a511c4a76
Vasily Myazin <vm@konspiredesign.com>
2020-04-26 03:05:20 -0400

    Update layout; get article files names (WIP)

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,29 @@
-let express = require('express');
-let router = express.Router();
-let md = require('markdown-it')({ html: true});
+const express = require('express');
+const router = express.Router();
+const md = require('markdown-it')({ html: true});
 
-let fs = require('fs');
+const fs = require('fs');
+const fsPromises = fs.promises;
+const testFolder = './blog/articles';
+
+function listDir() {
+  try {
+    return fsPromises.readdir(testFolder);
+  } catch (err) {
+    console.error('Error occured while reading directory', err);
+  }
+}
+
+let listOfArticleFiles = listDir();
+function getArticles() {
+  listOfArticleFiles.then(function(result) {
+    console.log(result); // I need this outside the function
+    return result;
+  });
+};
+
+getArticles();
+
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Markdown Blog' });
@@ -15,13 +36,14 @@ router.get('/about', (req, res) => {
 router.get('/blog', (req, res) => {
   let blogData = fs.readFileSync('blog/articles/article1.md', 'utf8');
   let content = md.render(blogData);
+
   console.log(content);
   res.render('blog', { content });
 });
 
 router.get('/blog/:name', (req, res) => {
   let blogData = fs.readFileSync('blog/articles/' + req.params.name + '.md', 'utf8');
-  
+
   const payload = Object.assign({});
   payload.content = md.render(blogData);
   payload.isBlog = true;
