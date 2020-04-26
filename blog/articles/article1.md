387e05abf13e3781d253be01d04421180cd43ec6
Vasily Myazin <vm@konspiredesign.com>
2020-04-26 00:42:28 -0400

    Add blog page with markdown capability

---
+++ blog/articles/article1.md
@@ -0,0 +1,5 @@
+# That's the title of my page
+
+The answer is text.
+
+![Moscow](https://static.themoscowtimes.com/image/1360/0c/moscow10296761280.jpg)
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,22 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+let md = require('markdown-it')();
 
-/* GET home page. */
+let fs = require('fs');
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Markdown Blog' });
 });
 
 router.get('/about', (req, res) => {
   res.render('about');
+});
+
+router.get('/blog', (req, res) => {
+  let blogData = fs.readFileSync('blog/articles/article1.md', 'utf8');
+  let content = md.render(blogData);
+  console.log(content);
+  res.render('blog', { content });
 });
 
 module.exports = router;
