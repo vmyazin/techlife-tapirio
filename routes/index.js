cb54bda610344a68266e2844668f41f29134849c
Vasily Myazin <vm@konspiredesign.com>
2020-04-26 01:24:45 -0400

    Implement markadown article routing

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let router = express.Router();
-let md = require('markdown-it')();
+let md = require('markdown-it')({ html: true});
 
 let fs = require('fs');
 
@@ -17,6 +17,19 @@ router.get('/blog', (req, res) => {
   let content = md.render(blogData);
   console.log(content);
   res.render('blog', { content });
+});
+
+router.get('/blog/:name', (req, res) => {
+  let blogData = fs.readFileSync('blog/articles/' + req.params.name + '.md', 'utf8');
+  
+  const payload = Object.assign({});
+  payload.content = md.render(blogData);
+  payload.isBlog = true;
+  payload.path = req.path;
+  payload.layout = 'blog';
+  payload.isBlogPost = true;
+
+  res.render('article', payload);
 });
 
 module.exports = router;
