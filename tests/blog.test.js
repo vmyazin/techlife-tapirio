829340f0c67e334d710050bbc2aea5c793cb9259
Michael Makarov <info1117@gmail.com>
2020-05-10 22:51:17 -0400

    Adds test for /blog

--- tests/blog.test.js
+++ tests/blog.test.js
@@ -4,13 +4,13 @@ const router = require('../routes/index');
 const Compiler = require('../scripts/compiler')
 const path = router.blogPath;
 
-let compiler;
+let meta;
 describe('blog posts', () => {
     beforeAll(async () => {
-        compiler = new Compiler(path);
+        const compiler = new Compiler(path);
+        meta = await compiler.listMeta();
     })
     it('contains post image in each post', async () => {
-        const meta = await compiler.listMeta();
         for (let i = 0; i < meta.length; i++) {
             const response = await request(app)
                 .get(`/blog/${meta[i].slug}`);
@@ -19,17 +19,21 @@ describe('blog posts', () => {
         }
     })
 
-    it('contains post title in each post', async () => {
-        const meta = await compiler.listMeta();
+    it('contains all post titles on /blog', async () => {
+        const response = await request(app)
+            .get('/blog');
         for (let i = 0; i < meta.length; i++) {
-            const response = await request(app)
-            .get(`/blog/${meta[i].slug}`);
-            expect(response.text).toContain(meta[i].title);
+            expect(response.text).toContain(meta[i].image);
             expect(response.statusCode).toBe(200);
         }
     })
 
-    it('All blog posts compile', async () => {
-        expect(await compiler.compileAll()).toBe(true);
+    it('contains post title in each post', async () => {
+        for (let i = 0; i < meta.length; i++) {
+            const response = await request(app)
+                .get(`/blog/${meta[i].slug}`);
+            expect(response.text).toContain(meta[i].title);
+            expect(response.statusCode).toBe(200);
+        }
     })
 })
