4b3a175761ee4d2163e3e034f34549afd83033b7
Michael Makarov <info1117@gmail.com>
2020-05-21 23:55:16 -0400

    Apps add.js

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-const createError = require('http-errors');
-const sassMiddleware = require('node-sass-middleware');
-const express = require('express');
-const app = express();
-const basicConfig = require('./app.config')
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
+let sassMiddleware = require('node-sass-middleware');
 
-basicConfig(app);
+let indexRouter = require('./routes/index');
+let usersRouter = require('./routes/users');
 
+let app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'pages'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(sassMiddleware({
   src: __dirname + '/scss', //where the sass files are 
   dest: __dirname + '/public/stylesheets', //where css should go
@@ -16,17 +26,19 @@ app.use(sassMiddleware({
   outputStyle: 'compressed',
   prefix: '/stylesheets'
 }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use((req, res, next) => {
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use((err, req, res, next) => {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
