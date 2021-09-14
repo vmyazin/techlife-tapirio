12bdfc3b8ca7e843be83095d3aa8694fdd094201
Vasily Myazin <vasily@automotivemastermind.com>
2021-09-15 00:53:41 +0300

    Update about bg; fine tune styles

--- app.js
+++ app.js
@@ -1,54 +1,57 @@
-let createError = require('http-errors');
-let express = require('express');
-let path = require('path');
-let cookieParser = require('cookie-parser');
-let logger = require('morgan');
-let sassMiddleware = require('node-sass-middleware');
+let createError = require("http-errors");
+let express = require("express");
+let path = require("path");
+let cookieParser = require("cookie-parser");
+let logger = require("morgan");
+let sassMiddleware = require("node-sass-middleware");
 
-let indexRouter = require('./routes/index');
-let usersRouter = require('./routes/users');
+let indexRouter = require("./routes/index");
+let usersRouter = require("./routes/users");
 
 let app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'pages'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "pages"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(sassMiddleware({
-  src: __dirname + '/scss', //where the sass files are
-  dest: __dirname + '/public/stylesheets', //where css should go
-  debug: true,
-  indentedSyntax: false,
-  outputStyle: 'compressed',
-  prefix: '/stylesheets'
-}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  sassMiddleware({
+    src: __dirname + "/scss", //where the sass files are
+    dest: __dirname + "/public/stylesheets", //where css should go
+    debug: true,
+    indentedSyntax: false,
+    outputStyle: "compressed",
+    prefix: "/stylesheets",
+  })
+);
+app.use(express.static(path.join(__dirname, "public")));
 
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 //module.exports = app;
 app.listen(3000, () => {
-  console.log(`Example app listening at http://localhost:3000`)
+  console.log(`Example app listening at http://localhost:3000`);
 });
+
+module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,193 +1,236 @@
-const cors = require('cors')
-const express = require('express');
+const cors = require("cors");
+const express = require("express");
 const router = express.Router();
-router.blogPath = __dirname + '/../content/articles/';
-const Project = require('../scripts/app.functions');
+router.blogPath = __dirname + "/../content/articles/";
+const Project = require("../scripts/app.functions");
 const project = new Project(router.blogPath, {
-  podcastFeedXml: __dirname + '/../public/podcast-feed.xml'
+  podcastFeedXml: __dirname + "/../public/podcast-feed.xml",
 });
-const { parse } = require('node-html-parser');
+const { parse } = require("node-html-parser");
 
-const moment = require('moment');
-require('moment/locale/ru');
+const moment = require("moment");
+require("moment/locale/ru");
 
 // default hero image
 const defaultImage = "/images/bg-photo-02.jpg";
 
 let projectInfo = project.info;
 let podcast = {},
-    episodes = {}
+  episodes = {};
 
 let dateObj = new Date();
 let current = {};
 current.year = dateObj.getFullYear();
-projectInfo['currentYear'] = current.year;
+projectInfo["currentYear"] = current.year;
 
 project.init().then(() => {
-  project.sortBy({ property: "date", asc: false })
-  
+  project.sortBy({ property: "date", asc: false });
+
   podcast = project.podcastModule.json.rss;
-  episodes = podcast.channel.item.map(episode => {
+  episodes = podcast.channel.item.map((episode) => {
     let episodeNumber = episode.title.split(":")[0];
     // add clean episode number to object
-    episode.episodeNum = episodeNumber.replace("#","");
+    episode.episodeNum = episodeNumber.replace("#", "");
     // add clean episode title
-    episode.title = episode.title.replace(episodeNumber + ": ", ""); 
+    episode.title = episode.title.replace(episodeNumber + ": ", "");
     // add neat episode date in Russian
-    episode.pubDateConverted = moment(episode.pubDate).locale('ru').format("LL"); 
+    episode.pubDateConverted = moment(episode.pubDate)
+      .locale("ru")
+      .format("LL");
 
     // get first available image for sharing
-    const root = parse(episode.description)
-    img = root.querySelector('img')
+    const root = parse(episode.description);
+    img = root.querySelector("img");
     if (img !== null) {
-      const url = img.getAttribute('src')
-      episode.shareImg = url
+      const url = img.getAttribute("src");
+      episode.shareImg = url;
     }
 
     return episode;
-  })
+  });
 });
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   const articles = project.posts;
-  res.render('index', { podcast, articles, projectInfo, isHeroParallax: true, heroImg: defaultImage, path: "home" });
+  res.render("index", {
+    podcast,
+    articles,
+    projectInfo,
+    isHeroParallax: true,
+    heroImg: defaultImage,
+    path: "home",
+  });
 });
 
-router.get('/home-2021', (req, res) => {
-  res.render('home-2021', {
+router.get("/home-2021", (req, res) => {
+  res.render("home-2021", {
     podcast,
     projectInfo,
     path: req.path,
-    pageTitle: 'О нас',
+    pageTitle: "О нас",
     isHeroParallax: true,
-    heroImg: '/images/bg-techlife-kamas.jpg'
- });
-});
-
-router.get('/about', (req, res) => {
-  res.render('about', {
-    projectInfo,
-    path: req.path,
-    pageTitle: 'О нас',
-    isHeroParallax: true,
-    heroImg: '/images/bg-lightning.jpg',
-    pageDescription: 'Авторы Дмитрий Здоров и Василий Мязин давние друзья и записывают подкаст о технологиях часто находясь в разных странах'
- });
-});
-
-router.get('/resources', (req, res) => {
-  res.render('resources', {
-    projectInfo,
-    path: req.path,
-    isHeroParallax: true,
-    pageTitle: 'Ресурсы',
-    heroImg: '/images/bg-lightning.jpg',
-    pageDescription: 'Дополнительные материалы в качестве приложения к подкасту; статьи, картинки, ссылки и т. п.'
+    heroImg: "/images/bg-techlife-kamas.jpg",
   });
 });
 
-router.get('/guests', (req, res) => {
-  res.render('guests', {
+router.get("/about", (req, res) => {
+  res.render("about", {
     projectInfo,
     path: req.path,
+    pageTitle: "О нас",
     isHeroParallax: true,
-    pageTitle: 'Инструкции для гостей подкаста',
-    pageDescription: 'Если вас пригласили на подкаст в гости, вам надо подготовится. Мы объясняем как это сделать.',
-    heroImg: '',
-    pageShareImg: '/images/og-techlife-guests-1200.jpg'
+    heroImg: "/images/bg-techlife-kamas.jpg",
+    pageDescription:
+      "Авторы Дмитрий Здоров и Василий Мязин давние друзья и записывают подкаст о технологиях часто находясь в разных странах",
   });
 });
 
-router.route('/api/episode/:id').get(cors(), async (req, res) => {
+router.get("/resources", (req, res) => {
+  res.render("resources", {
+    projectInfo,
+    path: req.path,
+    isHeroParallax: true,
+    pageTitle: "Ресурсы",
+    heroImg: "/images/bg-lightning.jpg",
+    pageDescription:
+      "Дополнительные материалы в качестве приложения к подкасту; статьи, картинки, ссылки и т. п.",
+  });
+});
+
+router.get("/guests", (req, res) => {
+  res.render("guests", {
+    projectInfo,
+    path: req.path,
+    isHeroParallax: true,
+    pageTitle: "Инструкции для гостей подкаста",
+    pageDescription:
+      "Если вас пригласили на подкаст в гости, вам надо подготовится. Мы объясняем как это сделать.",
+    heroImg: "",
+    pageShareImg: "/images/og-techlife-guests-1200.jpg",
+  });
+});
+
+router.route("/api/episode/:id").get(cors(), async (req, res) => {
   if (req.params.id) {
-    result = episodes.find(obj => {
+    result = episodes.find((obj) => {
       return obj.episodeNum === req.params.id;
     });
   } else {
-    result = []
+    result = [];
   }
-  res.json(result)
-})
+  res.json(result);
+});
 
-router.get('/episodes/:id', async (req, res) => {
+router.get("/episodes/:id", async (req, res) => {
   const slug = req.params.id;
-  
+
   if (slug) {
-    const i = episodes.findIndex(obj => obj.episodeNum === slug);
+    const i = episodes.findIndex((obj) => obj.episodeNum === slug);
     const episode = episodes[i];
     const nextEpisode = episodes[i + 1];
     const prevEpisode = episodes[i - 1];
 
     if (episode) {
-      res.render('episode', Object.assign({},
-        { projectInfo },
-        {
-          episode, nextEpisode, prevEpisode,
-          path: req.path,
-          isEpisodePage: true,
-          isHeroParallax: true,
-          heroImg: defaultImage,
-          layout: 'episode'
-        }
-      )) 
+      res.render(
+        "episode",
+        Object.assign(
+          {},
+          { projectInfo },
+          {
+            episode,
+            nextEpisode,
+            prevEpisode,
+            path: req.path,
+            isEpisodePage: true,
+            isHeroParallax: true,
+            heroImg: defaultImage,
+            layout: "episode",
+          }
+        )
+      );
       return;
     }
-   }
-   res.render('error rendering');
-  });
-  
-router.get('/tags', async (req, res) => {
-  const tags = project.tags;
-  res.render('tags', { tags, isHeroParallax: true, heroImg: defaultImage, projectInfo, path: req.path });
+  }
+  res.render("error rendering");
 });
 
-router.get('/tags/:tag', async (req, res) => {
+router.get("/tags", async (req, res) => {
+  const tags = project.tags;
+  res.render("tags", {
+    tags,
+    isHeroParallax: true,
+    heroImg: defaultImage,
+    projectInfo,
+    path: req.path,
+  });
+});
+
+router.get("/tags/:tag", async (req, res) => {
   const tag = req.params.tag;
   const tags = project.tags;
   const articles = await project.getPostsByTag(tag);
-  res.render('tag', { tag, tags, isHeroParallax: true, heroImg: defaultImage, articles, projectInfo, path: req.path });
+  res.render("tag", {
+    tag,
+    tags,
+    isHeroParallax: true,
+    heroImg: defaultImage,
+    articles,
+    projectInfo,
+    path: req.path,
+  });
 });
 
-router.get('/blog', async (req, res) => {
+router.get("/blog", async (req, res) => {
   const articles = project.posts;
-  res.render('blog', { articles, projectInfo, isHeroParallax: true, heroImg: defaultImage, path: req.path });
+  res.render("blog", {
+    articles,
+    projectInfo,
+    isHeroParallax: true,
+    heroImg: defaultImage,
+    path: req.path,
+  });
 });
 
-router.route('/api/search').get(cors(), async (req, res) => {
+router.route("/api/search").get(cors(), async (req, res) => {
   const articles = project.posts;
-  const search = req.query.name.toLowerCase()
+  const search = req.query.name.toLowerCase();
 
   if (search) {
-    results = articles.filter((a) => (a.title + a.description + a.author).toLowerCase().includes(search));
+    results = articles.filter((a) =>
+      (a.title + a.description + a.author).toLowerCase().includes(search)
+    );
   } else {
-    results = []
+    results = [];
   }
-  res.json(results)
+  res.json(results);
 });
 
-
-router.get('/blog/:filename', async (req, res) => {
+router.get("/blog/:filename", async (req, res) => {
   const slug = req.params.filename;
   const postMetaData = project.getPostMetadata(slug);
 
   if (!postMetaData) {
-    res.render('blog-not-found', slug);
+    res.render("blog-not-found", slug);
     return;
   }
 
-  res.render('article', Object.assign({},
-    { postMetaData },
-    { projectInfo },
-    {
-      content: await project.renderMarkdown(slug),
-      path: req.path,
-      layout: 'blog',
-      isHeroParallax: true,
-      heroImg: defaultImage,
-      isBlog: true,
-      isBlogPost: true
-    }
-  ));
+  res.render(
+    "article",
+    Object.assign(
+      {},
+      { postMetaData },
+      { projectInfo },
+      {
+        content: await project.renderMarkdown(slug),
+        path: req.path,
+        layout: "blog",
+        isHeroParallax: true,
+        heroImg: defaultImage,
+        isBlog: true,
+        isBlogPost: true,
+      }
+    )
+  );
 });
 
 module.exports = router;
