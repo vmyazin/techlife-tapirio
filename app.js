c082b0778ebb73b043d71cde48a1d04c4630e0f7
Vasily Myazin <vm@konspiredesign.com>
2024-07-30 22:08:39 -0300

    Improve app.js with Claude

--- app.js
+++ app.js
@@ -1,23 +1,67 @@
-//  app.js
+// app.js
 
-let createError = require("http-errors");
-let express = require("express");
+const createError = require("http-errors");
+const express = require("express");
 const path = require("path");
-let cookieParser = require("cookie-parser");
-let logger = require("morgan");
-let sassMiddleware = require("sass-middleware");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const sassMiddleware = require("sass-middleware");
 
-let indexRouter = require("./routes/index");
-let usersRouter = require("./routes/users");
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 
-let app = express();
+const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "pages"));
 app.set("view engine", "pug");
 
+// Middleware setup
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(
+  sassMiddleware({
+    src: path.join(__dirname, "scss"),
+    dest: path.join(__dirname, "public/stylesheets"),
+    debug: true,
+    outputStyle: "compressed",
+    prefix: "/stylesheets",
+  })
+);
 app.use(express.static(path.join(__dirname, "public")));
 
+// Initialize project data
+const Project = require("./scripts/app.functions");
+const BLOG_PATH = path.join(__dirname, "content", "articles");
+const PODCAST_FEED_XML = path.join(__dirname, "public", "podcast-feed.xml");
+
+const project = new Project(BLOG_PATH, { podcastFeedXml: PODCAST_FEED_XML });
+
+// This should be an async function
+async function initializeProject() {
+  await project.init();
+  project.sortBy({ property: "date", asc: false });
+
+  const podcast = project.podcastModule.json.rss;
+  const episodes = podcast.channel.item.map((episode) => {
+    // Your episode mapping logic here
+    return episode;
+  });
+
+  // Set up locals after data is initialized
+  app.locals.episodes = episodes;
+  app.locals.projectInfo = project.info;
+  app.locals.projectInfo.currentYear = new Date().getFullYear();
+
+  console.log('Application initialization complete.');
+}
+
+// Call the initialization function
+initializeProject().catch(console.error);
+
+// Routes
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
@@ -29,56 +73,20 @@ app.use(function (req, res, next) {
     path: req.path || ''
   });
 });
-// error handler
+
+// Error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
-  // render the error page
   res.status(err.status || 500);
   res.render("error", {
     path: req.path || ''
   });
 });
 
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(
-  sassMiddleware({
-    src: path.join(__dirname, "scss"), // where the sass files are
-    dest: path.join(__dirname, "public/stylesheets"), // where css should go
-    debug: true,
-    outputStyle: "compressed",
-    prefix: "/stylesheets",
-  })
-);
-
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`App listening at http://localhost:${PORT}`);
 });
-
-console.log('Application initialization complete.');
 
 module.exports = app;
